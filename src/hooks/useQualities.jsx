0b43a1028e22f4283b2d4d085e0c48bef3dc9314
Mokhmad <mokhmad.tazbiev@gmail.com>
2021-11-11 14:09:29 +0300

    qualities-crud-patterns

---
+++ src/hooks/useQualities.jsx
@@ -0,0 +1,67 @@
+import React, { useContext, useEffect, useState } from "react";
+import qualityService from "../app/services/quality.service";
+
+const QualitiesContext = React.createContext();
+export const useQualities = () => {
+  return useContext(QualitiesContext);
+};
+
+export const QualitiesProvider = ({ children }) => {
+  const [qualities, setQualities] = useState([]);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const getQualities = async () => {
+      try {
+        const { content } = await qualityService.fetchAll();
+        setQualities(content);
+        setIsLoading(false);
+      } catch (err) {
+        const { message } = err.response.data;
+        setError(message);
+      }
+    };
+    getQualities();
+  }, []);
+
+  const getQuality = (id) => {
+    return qualities.find((q) => q._id === id);
+  };
+
+  const updateQuality = async ({ _id: id, ...data }) => {
+    try {
+      const { content } = await qualityService.update(id, data);
+      setQualities((prevState) =>
+        prevState.map((item) => {
+          if (item._id === content._id) {
+            return content;
+          }
+          return item;
+        })
+      );
+      return content;
+    } catch (error) {
+      const { message } = error.response.data;
+      setError(message);
+    }
+  };
+
+  const addQuality = async (data) => {
+    try {
+      const { content } = await qualityService.create(data);
+      setQualities((prevState) => [...prevState, content]);
+      return content;
+    } catch (err) {
+      const { message } = error.response.data;
+      setError(message);
+    }
+  };
+
+  return (
+    <QualitiesContext.Provider
+      value={{ qualities, getQuality, updateQuality, addQuality }}
+    >
+      {!isLoading ? children : "Loading Qualities ...."}
+    </QualitiesContext.Provider>
+  );
+};
